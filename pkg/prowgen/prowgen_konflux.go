509388823d7efc0942911c6036a7c0ee77211b79
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-06-19 13:36:27 +0200

    Prowgen: support passing directory to make the generation faster (#167)
    It takes over 20minutes to run.
    
    This should speed up the `make generate-ci` command as we won't
    run `make ci-operator-config jobs` multiple times (twice per
    file vs twice per directory)
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/prowgen/prowgen_konflux.go
+++ pkg/prowgen/prowgen_konflux.go
@@ -12,54 +12,56 @@ const (
 	KonfluxBranchPrefix = "sync-konflux/"
 )
 
-func GenerateKonflux(ctx context.Context, openshiftRelease Repository, config *Config) error {
+func GenerateKonflux(ctx context.Context, openshiftRelease Repository, configs []*Config) error {
 
-	for _, r := range config.Repositories {
-		for branchName, b := range config.Config.Branches {
-			if b.Konflux != nil && b.Konflux.Enabled {
+	for _, config := range configs {
+		for _, r := range config.Repositories {
+			for branchName, b := range config.Config.Branches {
+				if b.Konflux != nil && b.Konflux.Enabled {
 
-				if err := GitMirror(ctx, r); err != nil {
-					return err
-				}
+					if err := GitMirror(ctx, r); err != nil {
+						return err
+					}
 
-				if err := GitCheckout(ctx, r, branchName); err != nil {
-					return err
-				}
+					if err := GitCheckout(ctx, r, branchName); err != nil {
+						return err
+					}
 
-				// Special case "release-next"
-				targetBranch := branchName
-				upstreamVersion := "release-next"
-				if branchName == "release-next" {
-					targetBranch = "main"
-				} else {
-					upstreamVersion = sobranch.FromUpstreamVersion(branchName)
-				}
+					// Special case "release-next"
+					targetBranch := branchName
+					upstreamVersion := "release-next"
+					if branchName == "release-next" {
+						targetBranch = "main"
+					} else {
+						upstreamVersion = sobranch.FromUpstreamVersion(branchName)
+					}
 
-				cfg := konfluxgen.Config{
-					OpenShiftReleasePath: openshiftRelease.RepositoryDirectory(),
-					ApplicationName:      fmt.Sprintf("serverless-operator %s", upstreamVersion),
-					Includes: []string{
-						fmt.Sprintf("ci-operator/config/%s/.*%s.*.yaml", r.RepositoryDirectory(), branchName),
-					},
-					Excludes: nil,
-					ExcludesImages: []string{
-						".*source.*",
-						".*test.*",
-					},
-					ResourcesOutputPath: fmt.Sprintf("%s/.konflux", r.RepositoryDirectory()),
-					PipelinesOutputPath: fmt.Sprintf("%s/.tekton", r.RepositoryDirectory()),
-					Nudges:              b.Konflux.Nudges,
-				}
+					cfg := konfluxgen.Config{
+						OpenShiftReleasePath: openshiftRelease.RepositoryDirectory(),
+						ApplicationName:      fmt.Sprintf("serverless-operator %s", upstreamVersion),
+						Includes: []string{
+							fmt.Sprintf("ci-operator/config/%s/.*%s.*.yaml", r.RepositoryDirectory(), branchName),
+						},
+						Excludes: nil,
+						ExcludesImages: []string{
+							".*source.*",
+							".*test.*",
+						},
+						ResourcesOutputPath: fmt.Sprintf("%s/.konflux", r.RepositoryDirectory()),
+						PipelinesOutputPath: fmt.Sprintf("%s/.tekton", r.RepositoryDirectory()),
+						Nudges:              b.Konflux.Nudges,
+					}
 
-				if err := konfluxgen.Generate(cfg); err != nil {
-					return fmt.Errorf("failed to generate Konflux configurations for %s (%s): %w", r.RepositoryDirectory(), branchName, err)
-				}
+					if err := konfluxgen.Generate(cfg); err != nil {
+						return fmt.Errorf("failed to generate Konflux configurations for %s (%s): %w", r.RepositoryDirectory(), branchName, err)
+					}
 
-				pushBranch := fmt.Sprintf("%s%s", KonfluxBranchPrefix, branchName)
-				commitMsg := fmt.Sprintf("[%s] Sync Konflux configurations", targetBranch)
+					pushBranch := fmt.Sprintf("%s%s", KonfluxBranchPrefix, branchName)
+					commitMsg := fmt.Sprintf("[%s] Sync Konflux configurations", targetBranch)
 
-				if err := PushBranch(ctx, r, nil, pushBranch, commitMsg); err != nil {
-					return err
+					if err := PushBranch(ctx, r, nil, pushBranch, commitMsg); err != nil {
+						return err
+					}
 				}
 			}
 		}
