42d06c7b9af51debdaf10b2fee7174d42d1d683a
rudyredhat1 <rpratap@redhat.com>
2024-09-12 15:59:35 +0530

    updated the req changes

--- pkg/konfluxgen/konfluxgen.go
+++ pkg/konfluxgen/konfluxgen.go
@@ -37,7 +37,7 @@ var PipelineDockerBuildTemplate embed.FS
 var PipelineFBCBuildTemplate embed.FS
 
 //go:embed integration-test-scenario.template.yaml
-var IntegrationTestScenarioTemplate embed.FS
+var EnterpriseContractTestScenarioTemplate embed.FS
 
 type Config struct {
 	OpenShiftReleasePath string
@@ -148,7 +148,7 @@ func Generate(cfg Config) error {
 	if err != nil {
 		return fmt.Errorf("failed to parse pipeline run push template: %w", err)
 	}
-	integrationTestScenarioTemplate, err := template.New("integration-test-scenario.template.yaml").Delims("{{{", "}}}").Funcs(funcs).ParseFS(IntegrationTestScenarioTemplate, "*.yaml")
+	integrationTestScenarioTemplate, err := template.New("integration-test-scenario.template.yaml").Delims("{{{", "}}}").Funcs(funcs).ParseFS(EnterpriseContractTestScenarioTemplate, "*.yaml")
 	if err != nil {
 		return fmt.Errorf("failed to parse integration test scenario template: %w", err)
 	}
@@ -272,7 +272,7 @@ func Generate(cfg Config) error {
 
 			buf.Reset()
 
-			ecTestPath := filepath.Join(cfg.ResourcesOutputPath, "tests", appKey, "ec-test.yaml")
+			ecTestPath := filepath.Join(cfg.ResourcesOutputPath, "applications", appKey, "tests", "ec-test.yaml")
 			if err := os.MkdirAll(filepath.Dir(ecTestPath), 0777); err != nil {
 				return fmt.Errorf("failed to create directory for %q: %w", appPath, err)
 			}
@@ -280,9 +280,11 @@ func Generate(cfg Config) error {
 			if err := integrationTestScenarioTemplate.Execute(buf, config); err != nil {
 				return fmt.Errorf("failed to execute template for EC test: %w", err)
 			}
-			if err := WriteFileReplacingNewerTaskImages(appPath, buf.Bytes(), 0777); err != nil {
+			if err := WriteFileReplacingNewerTaskImages(ecTestPath, buf.Bytes(), 0777); err != nil {
 				return fmt.Errorf("failed to write application file %q: %w", ecTestPath, err)
 			}
+
+			buf.Reset()
 
 			if config.Pipeline == FBCBuild {
 
