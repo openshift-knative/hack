6a8f1de291d7e7726282cfc847a548d6765ee24b
Christoph Stäbler <cstabler@redhat.com>
2025-07-16 11:43:40 +0200

    Generate OWNERS file from repo config file

---
+++ pkg/ownersfilegen/ownersfilegen.go
@@ -0,0 +1,47 @@
+package ownersfilegen
+
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"slices"
+	"text/template"
+)
+
+//go:embed owners.tmpl
+var OwnersTemplate embed.FS
+
+func WriteOwnersFile(repoDir string, reviewers, approvers []string) error {
+	buf := &bytes.Buffer{}
+
+	ownersTemplate, err := template.New("owners.tmpl").ParseFS(OwnersTemplate, "*.tmpl")
+	if err != nil {
+		return fmt.Errorf("failed to parse OWNERS template: %w", err)
+	}
+
+	sortedReviewers := make([]string, len(reviewers))
+	sortedApprovers := make([]string, len(approvers))
+	copy(sortedReviewers, reviewers)
+	copy(sortedApprovers, approvers)
+	slices.Sort(sortedReviewers)
+	slices.Sort(sortedApprovers)
+
+	d := map[string]interface{}{
+		"reviewers": sortedReviewers,
+		"approvers": sortedApprovers,
+	}
+
+	if err := ownersTemplate.Execute(buf, d); err != nil {
+		return fmt.Errorf("failed to execute OWNERS file template: %w", err)
+	}
+
+	ownersFilePath := filepath.Join(repoDir, "OWNERS")
+	if err := os.WriteFile(ownersFilePath, buf.Bytes(), fs.ModePerm); err != nil {
+		return fmt.Errorf("failed to write OWNERS file %q: %w", ownersFilePath, err)
+	}
+
+	return nil
+}
